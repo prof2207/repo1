0445a940a8f6434e410f486e69acf40a8925cb89
Roman Punko <prof2207@mail.ru>
2022-03-26 23:15:24 +0300

    add lesson02

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-let title;
+let title = "Project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 20;
+let rollback = 50;
+let fullPrice = 100;
+let adaptive = true;
 
-let screens;
+console.log(typeof (title));
+console.log(typeof (fullPrice));
+console.log(typeof (adaptive));
+console.log(screens.length);
 
-let screenPrice;
+let price1 = " Стоимость верстки экранов ";
+let price2 = " рублей / долларов / гривен / юани";
+let price = price1 + screenPrice + price2;
+console.log(price);
 
-let rollback;
+let full1 = " Стоимость разработки сайта ";
+let full2 = " рублей / долларов / гривен / юани";
+let full = full1 + fullPrice + full2;
+console.log(full);
 
-let fullPrice;
+console.log(screens.toLowerCase().split(", "));
 
-let adaptive;
+let otkat1 = "Процент отката посреднику за работу";
+let otkat2 = fullPrice * (rollback / 100);
+let otkat = otkat1 + otkat2;
+console.log(otkat);
 
-alert("начать первый урок?");
 
-console.log("privet");
 
-// change
-// change2
-// change3
+
+
+
+
+
