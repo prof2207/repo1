79972d7bcdd183460ed7dcd9e5bab27105b2f999
Roman Punko <prof2207@mail.ru>
2022-03-30 23:49:57 +0300

    lesson04

--- script.js
+++ script.js
@@ -1,18 +1,70 @@
-let title;
+'use strict';
 
-let screens;
+let title = prompt("Как называется ваш проект?", " КаЛьКулятор Верстки");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+let rollback = 10;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+let allServicePrices;
 
-let screenPrice;
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+};
 
-let rollback;
+const getAllServicePrices = function () {
+    allServicePrices = servicePrice1 + servicePrice2;
+}
 
-let fullPrice;
+function getFullPricegetFullPrice() {
+    fullPrice = screenPrice + allServicePrices;
+}
 
-let adaptive;
+function getTitle() {
+    title = title.split('');
+    if (title[0] == " ") {
+        title.splice(0, 1);
+    }
+    title = String(title[0]).toUpperCase() + title.splice(1).join('').toLocaleLowerCase();
+    console.log(title);
+}
 
-alert("начать первый урок?");
+function getServicePercentPrices() {
+    servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+    console.log(servicePercentPrice);
+}
 
-console.log("privet");
+const getRollbackMessage = function (price) {
+    if (price >= 30000) {
+        return "Даем скидку в 10%";
+    } else if (price >= 15000 && price < 30000) {
+        return "Даем скидку в 5%";
+    } else if (price >= 0 && price < 15000) {
+        return "Скидка не предусмотрена";
+    } else {
+        return "Что то пошло не так";
+    }
+};
 
-// change
-// change2
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+getTitle();
+getFullPricegetFullPrice();
+console.log(getAllServicePrices);
+console.log(screens);
+console.log(getRollbackMessage());
+console.log(getServicePercentPrices());
+
+
+
+
+
+
+
